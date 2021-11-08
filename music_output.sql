0404476e1d972da99272b097f628bc99064bc72e
Masashi Hamaguchi <masashi.hamaguchi@keio.jp>
2021-11-08 20:08:10 +0900

    fix: music_output.sql

--- music_output.sql
+++ music_output.sql
@@ -7,5 +7,6 @@
 \copy artists to 'output/artists.csv' with csv header null as ''
 \copy members to 'output/members.csv' with csv header null as ''
 \copy cd to 'output/cd.csv' with csv header null as ''
+\copy music to 'output/music.csv' with csv header null as ''
 \copy tracks to 'output/tracks.csv' with csv header null as ''
 \copy plays to 'output/plays.csv' with csv header null as ''
